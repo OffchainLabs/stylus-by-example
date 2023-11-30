bde2d525e705b6969f30db12dfe5b866cec9123b
Chris Cordle <chrisco255@protonmail.com>
2023-11-30 05:48:26 -0600

    panda express

---
+++ panda.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from "@pandacss/dev";
+
+export default defineConfig({
+  // Whether to use css reset
+  preflight: true,
+
+  // Where to look for your css declarations
+  include: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
+
+  // Files to exclude
+  exclude: [],
+
+  // Useful for theme customization
+  theme: {
+    extend: {
+      tokens: {
+        fonts: {
+          sans: { value: "var(--font-sans), sans-serif" },
+          mono: { value: "var(--font-mono), monospace" },
+        },
+      },
+    },
+  },
+
+  jsxFramework: "react",
+
+  // The output directory for your css system
+  outdir: "./src/styled-system",
+});
