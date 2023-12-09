98379590626e097a62d49567f0c2a2ddb0e8d941
Chris Cordle <chrisco255@protonmail.com>
2023-12-08 20:00:07 -0600

    fixing typos and adding descriptions

--- src/data/routes.ts
+++ src/data/routes.ts
@@ -19,21 +19,25 @@ export const basicExamples = [
   {
     route: "/basic_examples/first_app",
     title: "First App",
-    description: "Learn how to use the console output",
+    description:
+      "Simple counter contract that defines a count value in storage that can be incremented",
   },
   {
     route: "/basic_examples/primitive_data_types",
     title: "Primitive Data Types",
-    description: "Learn how to use the console output",
+    description: "Learn about booleans, integers, and addresses",
   },
+  // TODO: Should add another page covering 'global' values like
+  // msg, block, etc.
   {
     route: "/basic_examples/variables",
     title: "Variables",
-    description: "Learn how to use the console output",
+    description: "Learn about local and storage variables",
   },
+  // TODO: SolBE has a 'Constants' page that covers what would be known as static variables in Rust
   {
     route: "/basic_examples/immutable",
     title: "Immutable",
-    description: "Learn how to use the console output",
+    description: "How to define static or immutable values in your contract",
   },
 ];
