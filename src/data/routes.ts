8b96cbd1d485d7394df682ce61847a2a5c89a108
Jason-Wanxt <wanxutao123@gmail.com>
2024-07-10 18:39:57 +0800

    resolve request changes

--- src/data/routes.ts
+++ src/data/routes.ts
@@ -7,8 +7,8 @@ export type RouteInfo = {
 
 export const gettingStarted = [
   {
-    route: '/getting_started/using_the_cli',
-    title: 'Using the CLI',
+    route: "/getting_started/using_the_cli",
+    title: "Using the CLI",
     description:
       "How to use cargo stylus and Foundry's cast to effectively test smart contracts",
   },
@@ -16,49 +16,49 @@ export const gettingStarted = [
 
 export const basicExamples = [
   {
-    route: '/basic_examples/hello_world',
-    title: 'Hello World',
-    description: 'Learn how to use the console output',
+    route: "/basic_examples/hello_world",
+    title: "Hello World",
+    description: "Learn how to use the console output",
   },
   {
-    route: '/basic_examples/bytes_in_bytes_out',
-    title: 'Bytes In, Bytes Out',
-    description: 'Learn about the basic entrypoint model',
+    route: "/basic_examples/bytes_in_bytes_out",
+    title: "Bytes In, Bytes Out",
+    description: "Learn about the basic entrypoint model",
   },
   {
-    route: '/basic_examples/first_app',
-    title: 'First App',
+    route: "/basic_examples/first_app",
+    title: "First App",
     description:
-      'Simple counter contract that defines a count value in storage that can be incremented',
+      "Simple counter contract that defines a count value in storage that can be incremented",
   },
   {
-    route: '/basic_examples/primitive_data_types',
-    title: 'Primitive Data Types',
-    description: 'Learn about booleans, integers, and addresses',
+    route: "/basic_examples/primitive_data_types",
+    title: "Primitive Data Types",
+    description: "Learn about booleans, integers, and addresses",
   },
   {
-    route: '/basic_examples/variables',
-    title: 'Variables',
-    description: 'Learn about local, state, and global variables',
+    route: "/basic_examples/variables",
+    title: "Variables",
+    description: "Learn about local, state, and global variables",
   },
   {
-    route: '/basic_examples/constants',
-    title: 'Constants',
-    description: 'How to define constant values in your contract',
+    route: "/basic_examples/constants",
+    title: "Constants",
+    description: "How to define constant values in your contract",
   },
   {
-    route: '/basic_examples/events',
-    title: 'Events',
-    description: 'Log public events to the blockchain',
+    route: "/basic_examples/events",
+    title: "Events",
+    description: "Log public events to the blockchain",
   },
   {
-    route: '/basic_examples/errors',
-    title: 'Errors',
-    description: 'Errors on Stylus Rust smart contracts',
+    route: "/basic_examples/errors",
+    title: "Errors",
+    description: "Errors on Stylus Rust smart contracts",
   },
   {
-    route: '/basic_examples/function_selector',
-    title: 'Function selector',
+    route: "/basic_examples/function_selector",
+    title: "Function selector",
     description:
       "Compute the encoded function selector of a contract's function",
   },
@@ -66,33 +66,33 @@ export const basicExamples = [
 
 export const applications = [
   {
-    route: '/applications/erc20',
-    title: 'ERC-20',
+    route: "/applications/erc20",
+    title: "ERC-20",
     description:
-      'An example implementation of the ERC-20 token standard in Rust',
+      "An example implementation of the ERC-20 token standard in Rust",
   },
   {
-    route: '/applications/erc721',
-    title: 'ERC-721',
+    route: "/applications/erc721",
+    title: "ERC-721",
     description:
-      'An example implementation of the ERC-721 token standard in Rust',
+      "An example implementation of the ERC-721 token standard in Rust",
   },
   {
-    route: '/applications/english_auction',
-    title: 'English Auction',
+    route: "/applications/english_auction",
+    title: "English Auction",
     description:
-      'An example implementation of the English Auction in Rust using Arbitrum Stylus.',
+      "An example implementation of the English Auction in Rust using Arbitrum Stylus.",
   },
   {
-    route: '/applications/multi_sig',
-    title: 'MultiSig',
+    route: "/applications/multi_sig",
+    title: "MultiSig",
     description:
-      'An example implementation of the MutiSig wallet in Rust using Arbitrum Stylus.',
+      "An example implementation of the MutiSig wallet in Rust using Arbitrum Stylus.",
   },
   {
-    route: '/applications/vending_machine',
-    title: 'Vending Machine',
+    route: "/applications/vending_machine",
+    title: "Vending Machine",
     description:
-      'An example implementation of the Vending Machine in Rust using Arbitrum Stylus.',
+      "An example implementation of the Vending Machine in Rust using Arbitrum Stylus.",
   },
 ];
