7d3eba3235cf87fbb6841c0b60c667d29cdf2aca
GreatSoshiant <61604245+GreatSoshiant@users.noreply.github.com>
2024-08-12 14:58:14 +0300

    adding code and readme to repo

---
+++ example_code/applications/multi_call/src/lib.rs
@@ -0,0 +1,47 @@
+#![cfg_attr(not(feature = "export-abi"), no_main)]
+extern crate alloc;
+
+#[global_allocator]
+static ALLOC: mini_alloc::MiniAlloc = mini_alloc::MiniAlloc::INIT;
+
+use alloy_primitives::U256;
+use alloy_sol_types::sol;
+use stylus_sdk::{abi::Bytes, alloy_primitives::Address, call::RawCall, prelude::*};
+
+#[solidity_storage]
+#[entrypoint]
+pub struct MultiCall;
+
+// Declare events and Solidity error types
+sol! {
+    error ArraySizeNotMatch();
+    error CallFailed(uint256 call_index);
+}
+
+#[derive(SolidityError)]
+pub enum MultiCallErrors {
+    ArraySizeNotMatch(ArraySizeNotMatch),
+    CallFailed(CallFailed),
+}
+
+#[external]
+impl MultiCall {
+    pub fn multicall(
+        &self,
+        addresses: Vec<Address>,
+        data: Vec<Bytes>,
+    ) -> Result<Vec<Bytes>, MultiCallErrors> {
+        let addr_len = addresses.len();
+        let data_len = data.len();
+        let mut results: Vec<Bytes> = Vec::new();
+        if addr_len != data_len {
+            return Err(MultiCallErrors::ArraySizeNotMatch(ArraySizeNotMatch {}));
+        }
+        for i in 0..addr_len {
+            let result = RawCall::new().call(addresses[i], data[i].to_vec().as_slice())
+                .map_err(|_| MultiCallErrors::CallFailed(CallFailed { call_index: U256::from(i) }))?;
+            results.push(result.into());
+}
+        Ok(results)
+    }
+}
