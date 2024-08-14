71b0d1e39d206e4201cf34eedd37c922695acc0a
GreatSoshiant <61604245+GreatSoshiant@users.noreply.github.com>
2024-08-14 16:27:01 +0300

    erc20 code updated

--- example_code/applications/erc20/src/main.rs
+++ example_code/applications/erc20/src/main.rs
@@ -1,4 +1,4 @@
 #[cfg(feature = "export-abi")]
 fn main() {
-    stylus_erc20::print_abi("MIT-OR-APACHE-2.0", "pragma solidity ^0.8.23;");
+    stylus_erc20_example::print_abi("MIT-OR-APACHE-2.0", "pragma solidity ^0.8.23;");
 }
