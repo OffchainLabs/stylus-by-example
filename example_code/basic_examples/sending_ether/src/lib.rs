e274e78b19e8bd4e77644a8bb293c7fc1d63c851
GreatSoshiant <61604245+GreatSoshiant@users.noreply.github.com>
2024-08-14 20:10:55 +0300

    tweak on sending ether

--- example_code/basic_examples/sending_ether/src/lib.rs
+++ example_code/basic_examples/sending_ether/src/lib.rs
@@ -1,5 +1,5 @@
-#![no_main]
-extern crate alloc;
+// Only run this as a WASM if the export-abi feature is not set.
+#![cfg_attr(not(any(feature = "export-abi", test)), no_main)]extern crate alloc;
 
 #[global_allocator]
 static ALLOC: mini_alloc::MiniAlloc = mini_alloc::MiniAlloc::INIT;
