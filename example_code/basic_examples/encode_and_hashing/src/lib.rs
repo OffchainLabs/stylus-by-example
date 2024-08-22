d4c0b751848738d8ca19c7b9e2fb7f393cb563fb
Jason-Wanxt <wanxutao123@gmail.com>
2024-08-22 15:44:23 +0800

    resolve request changes

--- example_code/basic_examples/encode_and_hashing/src/lib.rs
+++ example_code/basic_examples/encode_and_hashing/src/lib.rs
@@ -7,13 +7,12 @@ extern crate alloc;
 static ALLOC: mini_alloc::MiniAlloc = mini_alloc::MiniAlloc::INIT;
 
 /// Import items from the SDK. The prelude contains common traits and macros.
-use stylus_sdk::{alloy_primitives::{U256, Address, FixedBytes}, abi::Bytes, prelude::*};
+use stylus_sdk::{alloy_primitives::{U256, Address, FixedBytes}, abi::Bytes, prelude::*, crypto::keccak};
 use alloc::string::String;
 use alloc::vec::Vec;
 // Becauce the naming of alloy_primitives and alloy_sol_types is the same, so we need to re-name the types in alloy_sol_types
 use alloy_sol_types::{sol_data::{Address as SOLAddress, String as SOLString, Bytes as SOLBytes, *}, SolType};
 use alloy_sol_types::sol;
-use sha3::{Digest, Keccak256};
 
 // Define error
 sol! {
@@ -31,21 +30,6 @@ pub enum HasherError{
 sol_storage! {
     #[entrypoint]
     pub struct Hasher {
-    }
-}
-
-// Declare private method.
-impl Hasher {
-    fn keccak256(&self, data: Bytes) -> FixedBytes<32> {
-        // prepare hasher
-        let mut hasher = Keccak256::new();
-        // populate the data
-        hasher.update(data);
-        // hashing with keccack256
-        let result = hasher.finalize();
-        // convert the result hash to FixedBytes<32>
-        let result_vec = result.to_vec();
-        FixedBytes::<32>::from_slice(&result_vec)   
     }
 }
 
@@ -69,7 +53,7 @@ impl Hasher {
         // encode the tuple
         let tx_hash_data_encode = TxIdHashType::abi_encode_sequence(&tx_hash_data);
         // hash the encoded data
-        self.keccak256(tx_hash_data_encode.into())
+        keccak(tx_hash_data_encode).into()
     }
 
     // This should always return true
@@ -112,7 +96,7 @@ impl Hasher {
         // encode the tuple
         let tx_hash_data_encode_packed = TxIdHashType::abi_encode_packed(&tx_hash_data);
         // hash the encoded data
-        self.keccak256(tx_hash_data_encode_packed.into())
+        keccak(tx_hash_data_encode_packed).into()
     }
 
     // Packed encode the data and hash it, the same result with the above one
@@ -127,7 +111,7 @@ impl Hasher {
         // set the data to arrary and concat it directly
         let tx_hash_data_encode_packed = [&target.to_vec(), &value.to_be_bytes_vec(), func.as_bytes(), &data.to_vec(), &timestamp.to_be_bytes_vec()].concat();
         // hash the encoded data
-        self.keccak256(tx_hash_data_encode_packed.into())
+        keccak(tx_hash_data_encode_packed).into()
     }
 
 
@@ -141,10 +125,10 @@ impl Hasher {
         type TransferType = (SOLAddress, Uint<256>);
         let tx_data = (address, amount);
         let data = TransferType::abi_encode_sequence(&tx_data);
-         // Get function selector
-         let hashed_function_selector = self.keccak256(func.as_bytes().to_vec().into());
-         // Combine function selector and input data (use abi_packed way)
-         let calldata = [&hashed_function_selector[..4], &data].concat();
+        // Get function selector
+        let hashed_function_selector: FixedBytes<32> = keccak(func.as_bytes().to_vec()).into();
+        // Combine function selector and input data (use abi_packed way)
+        let calldata = [&hashed_function_selector[..4], &data].concat();
         calldata
     }
 
@@ -158,11 +142,10 @@ impl Hasher {
         type TransferType = (SOLAddress, Uint<256>);
         let tx_data = (address, amount);
         let data = TransferType::abi_encode_sequence(&tx_data);
-         // Get function selector
-         let hashed_function_selector = self.keccak256(func.as_bytes().to_vec().into());
-         // Combine function selector and input data (use abi_packed way)
-         let calldata = [&hashed_function_selector[..4], &data].concat();
-         self.keccak256(calldata.into())
+        // Get function selector
+        let hashed_function_selector: FixedBytes<32> = keccak(func.as_bytes().to_vec()).into();
+        // Combine function selector and input data (use abi_packed way)
+        let calldata = [&hashed_function_selector[..4], &data].concat();
+        keccak(calldata).into()
     }
-
 }
