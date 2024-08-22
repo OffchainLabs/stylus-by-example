1dd3d6e3b2458e5e80c38d58331649e30a7a2df6
Doug <4741454+douglance@users.noreply.github.com>
2024-08-22 15:11:25 -0400

    remove exta import

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,6 @@
 "use client";
 import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
-import dynamic from "next/dynamic";
 
 if (typeof window !== "undefined") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY ?? "", {
