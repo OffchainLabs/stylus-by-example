6989686338fd35d0001e802217ad4b12363374e2
GreatSoshiant <61604245+GreatSoshiant@users.noreply.github.com>
2024-09-03 10:34:22 -0700

    first app page updated

--- example_code/basic_examples/first_app/src/lib.rs
+++ example_code/basic_examples/first_app/src/lib.rs
@@ -1,105 +1,45 @@
-// Allow `cargo stylus export-abi` to generate a main function.
 #![cfg_attr(not(feature = "export-abi"), no_main)]
 extern crate alloc;
 
+use stylus_sdk::{alloy_primitives::U256, prelude::*, storage::StorageU256};
 
-/// Import items from the SDK. The prelude contains common traits and macros.
-use stylus_sdk::{alloy_primitives::{U256, Address, FixedBytes}, abi::Bytes, prelude::*};
-use alloc::string::String;
-// Becauce the naming of alloy_primitives and alloy_sol_types is the same, so we need to re-name the types in alloy_sol_types
-use alloy_sol_types::{sol_data::{Address as SOLAddress, String as SOLString, Bytes as SOLBytes, *}, SolType};
-use sha3::{Digest, Keccak256};
-
-// Define some persistent storage using the Solidity ABI.
-// `Encoder` will be the entrypoint.
+/// The solidity_storage macro allows this struct to be used in persistent
+/// storage. It accepts fields that implement the StorageType trait. Built-in
+/// storage types for Solidity ABI primitives are found under
+/// stylus_sdk::storage.
 #[storage]
+/// The entrypoint macro defines where Stylus execution begins. External methods
+/// are exposed by annotating an impl for this struct with #[external] as seen
+/// below.
 #[entrypoint]
-pub struct Encoder;
-
-impl Encoder {
-    fn keccak256(&self, data: Bytes) -> FixedBytes<32> {
-        // prepare hasher
-        let mut hasher = Keccak256::new();
-        // populate the data
-        hasher.update(data);
-        // hashing with keccack256
-        let result = hasher.finalize();
-        // convert the result hash to FixedBytes<32>
-        let result_vec = result.to_vec();
-        FixedBytes::<32>::from_slice(&result_vec)   
-    }
+pub struct Counter {
+    count: StorageU256,
 }
 
-/// Declare that `Encoder` is a contract with the following external methods.
+/// Define an implementation of the Counter struct, defining a set_count
+/// as well as inc and dec methods using the features of the Stylus SDK.
 #[public]
-impl Encoder {
-
-     // Encode the data and hash it
-     pub fn encode(
-        &self, 
-        target: Address,
-        value: U256,
-        func: String,
-        data: Bytes,
-        timestamp: U256
-    ) -> Vec<u8> {
-        // define sol types tuple
-        type TxIdHashType = (SOLAddress, Uint<256>, SOLString, SOLBytes, Uint<256>);
-        // set the tuple
-        let tx_hash_data = (target, value, func, data, timestamp);
-        // encode the tuple
-        let tx_hash_data_encode = TxIdHashType::abi_encode_sequence(&tx_hash_data);
-        tx_hash_data_encode
+impl Counter {
+    /// Gets the number from storage.
+    pub fn get(&self) -> Result<U256, Vec<u8>> {
+        Ok(self.count.get())
     }
 
-    // Packed encode the data and hash it, the same result with the following one
-    pub fn packed_encode(
-        &self, 
-        target: Address,
-        value: U256,
-        func: String,
-        data: Bytes,
-        timestamp: U256
-    )-> Vec<u8> {
-        // define sol types tuple
-        type TxIdHashType = (SOLAddress, Uint<256>, SOLString, SOLBytes, Uint<256>);
-        // set the tuple
-        let tx_hash_data = (target, value, func, data, timestamp);
-        // encode the tuple
-        let tx_hash_data_encode_packed = TxIdHashType::abi_encode_packed(&tx_hash_data);
-        tx_hash_data_encode_packed
+    /// Sets the count in storage to a user-specified value.
+    pub fn set_count(&mut self, count: U256) -> Result<(), Vec<u8>> {
+        self.count.set(count);
+        Ok(())
     }
 
-    // Packed encode the data and hash it, the same result with the above one
-    pub fn packed_encode_2(
-        &self, 
-        target: Address,
-        value: U256,
-        func: String,
-        data: Bytes,
-        timestamp: U256
-    )-> Vec<u8> {
-        // set the data to arrary and concat it directly
-        let tx_hash_data_encode_packed = [&target.to_vec(), &value.to_be_bytes_vec(), func.as_bytes(), &data.to_vec(), &timestamp.to_be_bytes_vec()].concat();
-        tx_hash_data_encode_packed
+    /// Increments count by 1
+    pub fn inc(&mut self) -> Result<(), Vec<u8>> {
+        let count = self.count.get() + U256::from(1);
+        self.set_count(count)
     }
 
-
-    // The func example: "transfer(address,uint256)"
-    pub fn encode_with_signature(
-        &self, 
-        func: String, 
-        address: Address, 
-        amount: U256
-    ) -> Vec<u8> {
-        type TransferType = (SOLAddress, Uint<256>);
-        let tx_data = (address, amount);
-        let data = TransferType::abi_encode_sequence(&tx_data);
-        // Get function selector
-        let hashed_function_selector = self.keccak256(func.as_bytes().to_vec().into());
-        // Combine function selector and input data (use abi_packed way)
-        let calldata = [&hashed_function_selector[..4], &data].concat();
-        calldata
+    /// Decrements count by 1
+    pub fn dec(&mut self) -> Result<(), Vec<u8>> {
+        let count = self.count.get() - U256::from(1);
+        self.set_count(count)
     }
-
 }
--- example_code/basic_examples/first_app/src/main.rs
+++ example_code/basic_examples/first_app/src/main.rs
@@ -2,5 +2,5 @@
 
 #[cfg(feature = "export-abi")]
 fn main() {
-    stylus_encode_hashing::print_abi("MIT-OR-APACHE-2.0", "pragma solidity ^0.8.23;");
+    stylus_counter::print_abi("MIT-OR-APACHE-2.0", "pragma solidity ^0.8.23;");
 }
