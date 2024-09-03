eaa0e81548d1352a668a92ae86ad74a8afaf8b95
GreatSoshiant <61604245+GreatSoshiant@users.noreply.github.com>
2024-09-03 08:59:12 -0700

    added and updated abi encode example

--- example_code/basic_examples/abi_encode/src/lib.rs
+++ example_code/basic_examples/abi_encode/src/lib.rs
@@ -1,59 +1,105 @@
-
-#![cfg_attr(not(any(feature = "export-abi", test)), no_main)]
+// Allow `cargo stylus export-abi` to generate a main function.
+#![cfg_attr(not(feature = "export-abi"), no_main)]
 extern crate alloc;
 
 
 /// Import items from the SDK. The prelude contains common traits and macros.
-use stylus_sdk::{alloy_primitives::{U256, Address}, prelude::*};
+use stylus_sdk::{alloy_primitives::{U256, Address, FixedBytes}, abi::Bytes, prelude::*};
+use alloc::string::String;
 // Becauce the naming of alloy_primitives and alloy_sol_types is the same, so we need to re-name the types in alloy_sol_types
-use alloy_sol_types::{sol_data::{Address as SOLAddress, *}, SolType};
-use alloy_sol_types::sol;
-
-// Define error
-sol! {
-    error DecodedFailed();
-}
-
-// Error types for the MultiSig contract
-#[derive(SolidityError)]
-pub enum DecoderError{
-    DecodedFailed(DecodedFailed)
-}
+use alloy_sol_types::{sol_data::{Address as SOLAddress, String as SOLString, Bytes as SOLBytes, *}, SolType};
+use sha3::{Digest, Keccak256};
 
 // Define some persistent storage using the Solidity ABI.
-// `Decoder` will be the entrypoint.
-sol_storage! {
-    #[entrypoint]
-    pub struct Decoder {
+// `Encoder` will be the entrypoint.
+#[storage]
+#[entrypoint]
+pub struct Encoder;
+
+impl Encoder {
+    fn keccak256(&self, data: Bytes) -> FixedBytes<32> {
+        // prepare hasher
+        let mut hasher = Keccak256::new();
+        // populate the data
+        hasher.update(data);
+        // hashing with keccack256
+        let result = hasher.finalize();
+        // convert the result hash to FixedBytes<32>
+        let result_vec = result.to_vec();
+        FixedBytes::<32>::from_slice(&result_vec)   
     }
 }
 
-
-/// Declare that `Decoder` is a contract with the following external methods.
+/// Declare that `Encoder` is a contract with the following external methods.
 #[public]
-impl Decoder {
-    // This should always return true
-    pub fn encode_and_decode(
+impl Encoder {
+
+     // Encode the data and hash it
+     pub fn encode(
         &self, 
-        address: Address, 
-        amount: U256
-    ) -> Result<bool, DecoderError> {
+        target: Address,
+        value: U256,
+        func: String,
+        data: Bytes,
+        timestamp: U256
+    ) -> Vec<u8> {
         // define sol types tuple
-        type TxIdHashType = (SOLAddress, Uint<256>);
+        type TxIdHashType = (SOLAddress, Uint<256>, SOLString, SOLBytes, Uint<256>);
         // set the tuple
-        let tx_hash_data = (address, amount);
+        let tx_hash_data = (target, value, func, data, timestamp);
         // encode the tuple
         let tx_hash_data_encode = TxIdHashType::abi_encode_sequence(&tx_hash_data);
+        tx_hash_data_encode
+    }
 
-        let validate = true;
-        
-        // Check the result
-        match TxIdHashType::abi_decode_sequence(&tx_hash_data_encode, validate) {
-            Ok(res) => Ok(res == tx_hash_data),
-            Err(_) => {
-                return Err(DecoderError::DecodedFailed(DecodedFailed{}));
-            },
-        }   
+    // Packed encode the data and hash it, the same result with the following one
+    pub fn packed_encode(
+        &self, 
+        target: Address,
+        value: U256,
+        func: String,
+        data: Bytes,
+        timestamp: U256
+    )-> Vec<u8> {
+        // define sol types tuple
+        type TxIdHashType = (SOLAddress, Uint<256>, SOLString, SOLBytes, Uint<256>);
+        // set the tuple
+        let tx_hash_data = (target, value, func, data, timestamp);
+        // encode the tuple
+        let tx_hash_data_encode_packed = TxIdHashType::abi_encode_packed(&tx_hash_data);
+        tx_hash_data_encode_packed
+    }
+
+    // Packed encode the data and hash it, the same result with the above one
+    pub fn packed_encode_2(
+        &self, 
+        target: Address,
+        value: U256,
+        func: String,
+        data: Bytes,
+        timestamp: U256
+    )-> Vec<u8> {
+        // set the data to arrary and concat it directly
+        let tx_hash_data_encode_packed = [&target.to_vec(), &value.to_be_bytes_vec(), func.as_bytes(), &data.to_vec(), &timestamp.to_be_bytes_vec()].concat();
+        tx_hash_data_encode_packed
+    }
+
+
+    // The func example: "transfer(address,uint256)"
+    pub fn encode_with_signature(
+        &self, 
+        func: String, 
+        address: Address, 
+        amount: U256
+    ) -> Vec<u8> {
+        type TransferType = (SOLAddress, Uint<256>);
+        let tx_data = (address, amount);
+        let data = TransferType::abi_encode_sequence(&tx_data);
+        // Get function selector
+        let hashed_function_selector = self.keccak256(func.as_bytes().to_vec().into());
+        // Combine function selector and input data (use abi_packed way)
+        let calldata = [&hashed_function_selector[..4], &data].concat();
+        calldata
     }
 
 }
