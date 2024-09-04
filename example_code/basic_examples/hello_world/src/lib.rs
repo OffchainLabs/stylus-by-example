4e6b45f038e513f82106855c31071f53fda769de
GreatSoshiant <61604245+GreatSoshiant@users.noreply.github.com>
2024-09-03 17:51:13 -0700

    format

--- example_code/basic_examples/hello_world/src/lib.rs
+++ example_code/basic_examples/hello_world/src/lib.rs
@@ -14,13 +14,11 @@ pub struct Hello {
 
 #[public]
 impl Hello {
-
-fn user_main(_input: Vec<u8>) -> ArbResult {
-    // Will print 'Stylus says: Hello Stylus!' on your local dev node
-    // Be sure to add "debug" feature flag to your Cargo.toml file as
-    // shown below.
-    console!("Hello Stylus!");
-
-    Ok(Vec::new())
-}
+    fn user_main(_input: Vec<u8>) -> ArbResult {
+        // Will print 'Stylus says: Hello Stylus!' on your local dev node
+        // Be sure to add "debug" feature flag to your Cargo.toml file as
+        // shown below.
+        console!("Hello Stylus!");
+        Ok(Vec::new())
+    }
 }
