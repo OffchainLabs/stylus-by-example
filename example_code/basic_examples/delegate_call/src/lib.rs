65af063fcf815a920f9de2d99753b530358b4689
GreatSoshiant <61604245+GreatSoshiant@users.noreply.github.com>
2024-09-13 07:58:01 -0700

    delegate call code updated

--- example_code/basic_examples/delegate_call/src/lib.rs
+++ example_code/basic_examples/delegate_call/src/lib.rs
@@ -1,19 +1,52 @@
 #![cfg_attr(not(feature = "export-abi"), no_main)]
 extern crate alloc;
 
-use stylus_sdk::{console, prelude::*, stylus_proc::entrypoint, ArbResult};
+use alloy_sol_types::sol;
+use stylus_sdk::{
+    alloy_primitives::Address,
+    call::{delegate_call, RawCall},
+    prelude::*,
+};
 
 #[storage]
 #[entrypoint]
-pub struct Hello;
+
+pub struct ExampleContract;
+
+// Declare events and Solidity error types
+sol! {
+    error DelegateCallFailed();
+}
+
+#[derive(SolidityError)]
+pub enum DelegateCallErrors {
+    DelegateCallFailed(DelegateCallFailed),
+}
 
 #[public]
-impl Hello {
-    fn user_main(_input: Vec<u8>) -> ArbResult {
-        // Will print 'Stylus says: Hello Stylus!' on your local dev node
-        // Be sure to add "debug" feature flag to your Cargo.toml file as
-        // shown below.
-        console!("Hello Stylus!");
-        Ok(Vec::new())
+impl ExampleContract {
+    pub fn low_level_delegate_call(
+        &mut self,
+        calldata: Vec<u8>,
+        target: Address,
+    ) -> Result<Vec<u8>, DelegateCallErrors> {
+        unsafe {
+            let result = delegate_call(self, target, &calldata)
+                .map_err(|_| DelegateCallErrors::DelegateCallFailed(DelegateCallFailed {}))?;
+
+            Ok(result)
+        }
+    }
+    pub fn raw_delegate_call(
+        &mut self,
+        calldata: Vec<u8>,
+        target: Address,
+    ) -> Result<Vec<u8>, Vec<u8>> {
+        let data = RawCall::new_delegate() // configure a delegate call
+            .gas(2100) // supply 2100 gas
+            .limit_return_data(0, 32) // only read the first 32 bytes back
+            .call(target, &calldata)?;
+
+        Ok(data)
     }
 }
